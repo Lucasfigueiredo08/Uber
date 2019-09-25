5aee1b1c5a164366e6d2f5e64042c5ddba6d6737
Lucas Figueiredo <lukas.figueiredo14@gmail.com>
2019-09-25 01:46:42 -0300

    UberJava

---
+++ Uber2/app/src/main/java/com/uber/cursoandroid/jamiltondamasceno/uber/activity/CadastroActivity.java
@@ -0,0 +1,100 @@
+package com.uber.cursoandroid.jamiltondamasceno.uber.activity;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.uber.cursoandroid.jamiltondamasceno.uber.R;
+import com.uber.cursoandroid.jamiltondamasceno.uber.config.ConfiguracaoFirebase;
+import com.uber.cursoandroid.jamiltondamasceno.uber.model.Usuario;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    private TextInputEditText campoNome, campoEmail, campoSenha;
+    private Switch switchTipoUsuario;
+
+    private FirebaseAuth autenticacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        //Inicializar componentes
+        campoNome  = findViewById(R.id.editCadastroNome);
+        campoEmail = findViewById(R.id.editCadastroEmail);
+        campoSenha = findViewById(R.id.editCadastroSenha);
+        switchTipoUsuario = findViewById(R.id.switchTipoUsuario);
+
+    }
+
+    public void validarCadastroUsuario(View view){
+
+        //Recuperar textos dos campos
+        String textoNome  = campoNome.getText().toString();
+        String textoEmail = campoEmail.getText().toString();
+        String textoSenha = campoSenha.getText().toString();
+
+        if( !textoNome.isEmpty() ) {//verifica nome
+            if( !textoEmail.isEmpty() ) {//verifica e-mail
+                if( !textoSenha.isEmpty() ) {//verifica senha
+
+                    Usuario usuario = new Usuario();
+                    usuario.setNome( textoNome );
+                    usuario.setEmail( textoEmail );
+                    usuario.setSenha( textoSenha );
+                    usuario.setTipo( verificaTipoUsuario() );
+
+                    cadastrarUsuario( usuario );
+
+                }else {
+                    Toast.makeText(CadastroActivity.this,
+                            "Preencha a senha!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(CadastroActivity.this,
+                        "Preencha o email!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(CadastroActivity.this,
+                    "Preencha o nome!",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void cadastrarUsuario( Usuario usuario ){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if ( task.isSuccessful() ){
+                   String idUsuario = task.getResult().getUser().getUid();
+                   usuario.setId( idUsuario );
+                }
+
+            }
+        });
+
+    }
+
+    public String verificaTipoUsuario(){
+        return switchTipoUsuario.isChecked() ? "M" : "P" ;
+    }
+
+}
